2d5814822e996fff7e405f79f69f377ca7851f06
Joe Lea <joeleapublic@gmail.com>
2015-11-19 12:15:51 +0000

    Woop

--- java-tdd/src/main/java/RomanNumerals.java
+++ java-tdd/src/main/java/RomanNumerals.java
@@ -1,9 +1,16 @@
 import com.google.common.collect.ImmutableMap;
 
 public class RomanNumerals {
+    private final ImmutableMap<Integer, String> numerals = ImmutableMap.of(
+            5, "V",
+            1, "I"
+    );
+
     public String convertToNumeral(int number) {
-        if (number >= 5) {
-            return convertAndRecurse(number, 5);
+        for(int atomicNumber : numerals.keySet()) {
+            if (number >= atomicNumber) {
+                return convertAndRecurse(number, atomicNumber);
+            }
         }
         if (number >= 1) {
             return convertAndRecurse(number, 1);
@@ -16,10 +23,6 @@ public class RomanNumerals {
     }
 
     private String numeralFor(int number) {
-        ImmutableMap<Integer, String> numerals = ImmutableMap.of(
-                1, "I",
-                5, "V"
-        );
         return numerals.get(number);
     }
 }
