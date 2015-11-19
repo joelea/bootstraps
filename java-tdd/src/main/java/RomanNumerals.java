0f38d55b24c63c537a9cd9039d5dc5ac9a5e4c70
Joe Lea <joeleapublic@gmail.com>
2015-11-19 12:06:13 +0000

    Remove some code! yay! everyone loves doing that

--- java-tdd/src/main/java/RomanNumerals.java
+++ java-tdd/src/main/java/RomanNumerals.java
@@ -1,8 +1,5 @@
 public class RomanNumerals {
     public String convertToNumeral(int number) {
-        if(number == (5+2)) {
-            return convertToNumeral(5) + convertToNumeral(2);
-        }
         if(number >= 5) {
             return "V" + convertToNumeral(number - 5);
         }
