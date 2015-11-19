64ea254e56f28c0b71d9972b603cf7420c7bc076
Joe Lea <joeleapublic@gmail.com>
2015-11-19 11:57:38 +0000

    Make the test order more human readable

--- java-tdd/src/test/java/RomanNumeralsShould.java
+++ java-tdd/src/test/java/RomanNumeralsShould.java
@@ -12,12 +12,12 @@ public class RomanNumeralsShould {
     }
 
     @Test public void
-    convert_5_to_V() {
-        assertThat(romanNumerals.convertToNumeral(5), is("V"));
+    convert_2_to_II() {
+        assertThat(romanNumerals.convertToNumeral(2), is("II"));
     }
 
     @Test public void
-    convert_2_to_II() {
-        assertThat(romanNumerals.convertToNumeral(2), is("II"));
+    convert_5_to_V() {
+        assertThat(romanNumerals.convertToNumeral(5), is("V"));
     }
 }
