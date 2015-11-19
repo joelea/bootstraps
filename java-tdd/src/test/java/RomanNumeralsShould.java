9711e25787794e660588be2acd7f4120e1d69f6d
Joe Lea <joeleapublic@gmail.com>
2015-11-19 11:49:56 +0000

    Refactor test duplication

--- java-tdd/src/test/java/RomanNumeralsShould.java
+++ java-tdd/src/test/java/RomanNumeralsShould.java
@@ -4,14 +4,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class RomanNumeralsShould {
+    private final RomanNumerals romanNumerals = new RomanNumerals();
+
     @Test public void
     convert_1_to_I() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         assertThat(romanNumerals.convert(1), is("I"));
     }
+
     @Test public void
     convert_5_to_V() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         assertThat(romanNumerals.convert(5), is("V"));
     }
 }
