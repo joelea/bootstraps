616c4e055a5a42c0991db8afba80ec348fc02b7e
Joe Lea <joeleapublic@gmail.com>
2015-11-19 12:11:29 +0000

    Extract out edge cases

--- java-tdd/src/main/java/RomanNumerals.java
+++ java-tdd/src/main/java/RomanNumerals.java
@@ -1,11 +1,21 @@
+import com.google.common.collect.ImmutableMap;
+
 public class RomanNumerals {
     public String convertToNumeral(int number) {
-        if(number >= 5) {
-            return "V" + convertToNumeral(number - 5);
+        if (number >= 5) {
+            return numeralFor(5) + convertToNumeral(number - 5);
         }
         if (number >= 1) {
-            return "I" + convertToNumeral(number - 1);
+            return numeralFor(1) + convertToNumeral(number - 1);
         }
         return "";
+    }
+
+    private String numeralFor(int number) {
+        ImmutableMap<Integer, String> numerals = ImmutableMap.of(
+                1, "I",
+                5, "V"
+        );
+        return numerals.get(number);
     }
 }
