f7e699edf2202af32e88a062dec6c2f5b4ab2a61
Joe Lea <joeleapublic@gmail.com>
2015-11-26 10:44:00 +0000

    Good starting point

--- java-tdd/src/test/java/RomanNumeralsShould.java
+++ java-tdd/src/test/java/RomanNumeralsShould.java
@@ -10,38 +10,4 @@ public class RomanNumeralsShould {
     convert_0_to_empty_string() {
         assertThat(romanNumerals.convertToNumeral(0), is(""));
     }
-
-    @Test public void
-    convert_1_to_I() {
-        assertThat(romanNumerals.convertToNumeral(1), is("I"));
-    }
-
-    @Test public void
-    convert_2_to_II() {
-        assertThat(romanNumerals.convertToNumeral(2), is("II"));
-    }
-
-    @Test public void
-    convert_4_to_IV() {
-        assertThat(romanNumerals.convertToNumeral(4), is("IV"));
-    }
-    @Test public void
-    convert_5_to_V() {
-        assertThat(romanNumerals.convertToNumeral(5), is("V"));
-    }
-
-    @Test public void
-    convert_7_To_VII() {
-        assertThat(romanNumerals.convertToNumeral(7), is("VII"));
-    }
-
-    @Test public void
-    convert_9_To_IX() {
-        assertThat(romanNumerals.convertToNumeral(9), is("IX"));
-    }
-
-    @Test public void
-    convert_10_To_X() {
-        assertThat(romanNumerals.convertToNumeral(10), is("X"));
-    }
 }
